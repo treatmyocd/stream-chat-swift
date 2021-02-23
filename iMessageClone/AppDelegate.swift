7bc0c1805d9eed5e17f5b2700d9886615d6551a1
marek.fort <marek.fort@ackee.cz>
2021-02-23 12:33:36 +0100

    Remove storyboard.

--- iMessageClone/AppDelegate.swift
+++ iMessageClone/AppDelegate.swift
@@ -1,8 +1,4 @@
 //
-//  AppDelegate.swift
-//  iMessageClone
-//
-//  Created by Marek Fořt on 23.02.2021.
 //  Copyright © 2021 Stream.io Inc. All rights reserved.
 //
 
@@ -10,28 +6,13 @@ import UIKit
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
-
-
+    var window: UIWindow?
+    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window?.makeKeyAndVisible()
+        window?.rootViewController = ViewController()
         return true
     }
-
-    // MARK: UISceneSession Lifecycle
-
-    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
-        // Called when a new scene session is being created.
-        // Use this method to select a configuration to create the new scene with.
-        return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
-    }
-
-    func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
-        // Called when the user discards a scene session.
-        // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
-        // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
-    }
-
-
 }
 
--- iMessageClone/ViewController.swift
+++ iMessageClone/ViewController.swift
@@ -1,17 +1,14 @@
 //
-//  ViewController.swift
-//  iMessageClone
-//
-//  Created by Marek Fořt on 23.02.2021.
 //  Copyright © 2021 Stream.io Inc. All rights reserved.
 //
 
 import UIKit
 
-class ViewController: UIViewController {
+final class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        view.backgroundColor = .red
     }
 }
 
