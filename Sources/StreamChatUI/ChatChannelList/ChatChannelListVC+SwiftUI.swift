aa2c9c8007a6d74a72212b958b20eaaee0a21302
Igor Rosocha <igor.rosocha@ackee.cz>
2021-02-24 15:11:42 +0100

    Implement ChatChannelListItemVC SwiftUI wrapper.

---
+++ Sources/StreamChatUI/ChatChannelList/ChatChannelListVC+SwiftUI.swift
@@ -0,0 +1,33 @@
+//
+//  ChatChannelListVC+SwiftUI.swift
+//  StreamChatUI
+//
+//  Created by Igor Rosocha on 2/24/21.
+//  Copyright © 2021 Stream.io Inc. All rights reserved.
+//
+
+import SwiftUI
+import StreamChat
+import UIKit
+
+@available(iOS 14.0, *)
+/// A `UIViewControllerRepresentable` subclass which wraps `ChatChannelListVC`.
+public struct ChatChannelListItemVCComponent<ExtraData: ExtraDataTypes>: UIViewControllerRepresentable {
+    /// The `ChatChannelListController` instance that provides channels data.
+    var controller: ChatChannelListController
+    
+    public init(controller: ChatChannelListController) {
+        self.controller = controller
+    }
+    
+    public func makeUIViewController(context: Context) -> _ChatChannelListVC<NoExtraData> {
+        let vc = _ChatChannelListVC<NoExtraData>()
+        vc.controller = controller
+
+        return vc
+    }
+    
+    public func updateUIViewController(_ chatChannelListVC: _ChatChannelListVC<NoExtraData>, context: Context) {
+
+    }
+}
