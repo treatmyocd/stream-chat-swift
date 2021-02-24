9fc0553d3ad9363f962a23d00ca607a15025b501
marek.fort <marek.fort@ackee.cz>
2021-02-24 15:48:05 +0100

    Disable slash suggestions.

---
+++ iMessageClone/iMessageChatComposerViewController.swift
@@ -0,0 +1,12 @@
+//
+//  Copyright © 2021 Stream.io Inc. All rights reserved.
+//
+
+import UIKit
+import StreamChat
+import StreamChatUI
+
+final class iMessageChatComposerViewController: ChatMessageComposerVC {
+    override func promptSuggestionIfNeeded(for text: String) {
+    }
+}
