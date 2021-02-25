b6d79ecc9c2e7e520d55c8ace073a804df31c49a
Igor Rosocha <igor.rosocha@ackee.cz>
2021-02-25 17:21:01 +0100

    Change implementation of CustomChannelListItemView into SwiftUI.

--- SwiftUIDemoApp/CustomChannelListItemView.swift
+++ SwiftUIDemoApp/CustomChannelListItemView.swift
@@ -8,17 +8,38 @@
 
 import StreamChat
 import StreamChatUI
-import UIKit
+import SwiftUI
 
-final class CustomChannelListItemView: _ChatChannelListItemView<NoExtraData> {
-    override init(frame: CGRect) {
-        super.init(frame: .zero)
-        
-        subtitleLabel.isHidden = true
-        timestampLabel.isHidden = true
-    }
+@available(iOS 14, *)
+public struct CustomChannelListItemView: ChatChannelListItemViewSwiftUIView {
+    public var title: String = ""
+    public var subtitle: String = ""
+    public var timestamp: String = ""
+    public var avatarImage: UIImage = UIImage(systemName: "person")!
     
-    required init?(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
+    public init() { }
+    
+    public var body: some View {
+        HStack(spacing: 10) {
+            Image(uiImage: avatarImage)
+                .padding(10)
+                .background(Color.gray.opacity(0.2))
+                .cornerRadius(20)
+            VStack(alignment: .leading) {
+                Text(title)
+                    .fontWeight(.medium)
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                    .offset(x: 10)
+                Text(subtitle)
+                    .font(.caption)
+                    .foregroundColor(.gray)
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                    .offset(x: 10)
+            }
+            Text(timestamp)
+                .font(.caption)
+                .foregroundColor(.gray)
+        }
+        .padding(EdgeInsets(top: 10, leading: 20, bottom: 10, trailing: 20))
     }
 }
