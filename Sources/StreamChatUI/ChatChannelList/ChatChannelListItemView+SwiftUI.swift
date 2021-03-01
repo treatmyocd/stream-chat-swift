5dc205bbc16b74b4663a05fbdc9e3403fb4c8af9
Igor Rosocha <igor.rosocha@ackee.cz>
2021-03-01 16:11:57 +0100

    Add SwiftUI as an extension of _ChatChannelListItemViewBase.

--- Sources/StreamChatUI/ChatChannelList/ChatChannelListItemView+SwiftUI.swift
+++ Sources/StreamChatUI/ChatChannelList/ChatChannelListItemView+SwiftUI.swift
@@ -24,24 +24,26 @@ public class _ChatChannelListItemView_DataSource<ExtraData: ExtraDataTypes>: Obs
 }
 
 @available(iOS 14, *)
-public class _ChatChannelListItemView_SwiftUI<Content: ChatChannelListItemViewSwiftUI_, ExtraData>:
-    _ChatChannelListItemViewBase<ExtraData> where Content.ExtraData == ExtraData
-{
-    var hostingController: UIHostingController<Content>?
-
-    lazy var dataSource = _ChatChannelListItemView_DataSource<ExtraData>()
-
-    override public func setUp() {
-        let view = Content.init(dataSource: self.dataSource)
-        hostingController = UIHostingController(rootView: view)
-    }
-
-    override public func setUpLayout() {
-        embed(hostingController!.view)
-    }
-
-    override public func updateContent() {
-        self.dataSource.channel = content.channel
-        self.dataSource.currentUserId = content.currentUserId
+extension _ChatChannelListItemViewBase {
+    public class SwiftUI<Content: ChatChannelListItemViewSwiftUI_, ExtraData>:
+        _ChatChannelListItemViewBase<ExtraData> where Content.ExtraData == ExtraData
+    {
+        var hostingController: UIHostingController<Content>?
+        
+        lazy var dataSource = _ChatChannelListItemView_DataSource<ExtraData>()
+        
+        override public func setUp() {
+            let view = Content.init(dataSource: self.dataSource)
+            hostingController = UIHostingController(rootView: view)
+        }
+        
+        override public func setUpLayout() {
+            embed(hostingController!.view)
+        }
+        
+        override public func updateContent() {
+            self.dataSource.channel = content.channel
+            self.dataSource.currentUserId = content.currentUserId
+        }
     }
 }
