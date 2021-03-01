22e52c751ce3f1ee5b337d62905a554d6549dc74
marek.fort <marek.fort@ackee.cz>
2021-03-01 16:03:37 +0100

    Create Slack clone.

---
+++ SlackClone/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  Copyright © 2021 Stream.io Inc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    var window: UIWindow?
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
