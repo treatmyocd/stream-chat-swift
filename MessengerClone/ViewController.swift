8bb19ee52f7f4baef7fc7d521b2fe9ec6feb04b9
marek.fort <marek.fort@ackee.cz>
2021-03-03 14:18:41 +0100

    Create Messenger clone

---
+++ MessengerClone/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MessengerClone
+//
+//  Created by Marek Fořt on 03.03.2021.
+//  Copyright © 2021 Stream.io Inc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
